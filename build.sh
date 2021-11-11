f8260c9cdd23d7420c49a328a3e74d9ad7a63bca
Killaship <69988679+Killaship@users.noreply.github.com>
2021-11-11 18:47:13 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
+if test "`whoami`" != "root" ; then
+	echo "You must be logged in as root to build (for loopback mounting)"
+	echo "Enter 'su' or 'sudo bash' to switch to root"
+	exit
+fi
+
 git pull
 nasm -fbin src/boot.asm -o boot
 qemu-system-i386 -fda boot
+bash mOSbuild.sh
