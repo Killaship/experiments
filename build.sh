31af59a22522d39a55fbb7832fe2813e6eef61da
Killaship <69988679+Killaship@users.noreply.github.com>
2021-11-12 15:06:45 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+rm boot
+rm mikeos.flp
 if test "`whoami`" != "root" ; then
 	echo "You must be logged in as root to build (for loopback mounting)"
 	echo "Enter 'su' or 'sudo bash' to switch to root"
@@ -11,3 +13,4 @@ bash mOSbuild.sh
 
 
 qemu-system-i386 -fda boot
+
