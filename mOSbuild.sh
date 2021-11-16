f88d2357fb3eb8d0170a403cc24863f90f3b7517
Killaship <69988679+Killaship@users.noreply.github.com>
2021-11-15 21:38:33 -0500

    Update mOSbuild.sh

--- mOSbuild.sh
+++ mOSbuild.sh
@@ -34,18 +34,32 @@ cd src
 nasm -O0 -w+orphan-labels -f bin -o kernel.bin kernel.asm || exit
 cd ..
 
+echo ">>> Assembling programs..."
 
+cd programs
+
+for i in *.asm
+do
+	nasm -O0 -w+orphan-labels -f bin $i -o `basename $i .asm`.bin || exit
+done
+
+cd ..
 
 echo ">>> Adding bootloader to floppy image..."
 
 dd status=noxfer conv=notrunc if=src/boot.bin of=mikeos.flp || exit
 
-
-echo ">>> Copying MikeOS kernel..."
+echo ">>> Copying MikeOS kernel and programs..."
 
 rm -rf tmp-loop
 
-mkdir tmp-loop && mount -o loop -t vfat mikeos.flp tmp-loop && cp src/kernel.bin tmp-loop/
+mkdir tmp-loop && mount -o loop -t vfat disk_images/mikeos.flp tmp-loop && cp source/kernel.bin tmp-loop/
+
+cp programs/*.bin programs/*.bas programs/sample.pcx tmp-loop
+
+sleep 0.2
+
+
 
 
 echo ">>> Unmounting loopback floppy..."
