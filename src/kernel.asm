fd2544fcb6b619fd9aa430b9470e780f208ccfb9
Killaship <69988679+Killaship@users.noreply.github.com>
2021-11-16 20:50:34 -0500

    Update kernel.asm

--- src/kernel.asm
+++ src/kernel.asm
@@ -14,6 +14,19 @@ start:
 	mov es, ax			; segments ever again, as MikeOS and its programs
 	mov fs, ax			; live entirely in 64K
 	mov gs, ax
+	
+	cmp dl, 0
+	je no_change
+	mov [bootdev], dl		; Save boot device number
+	push es
+	mov ah, 8			; Get drive parameters
+	int 13h
+	pop es
+	and cx, 3Fh			; Maximum sector number
+	mov [SecsPerTrack], cx		; Sector numbers start at 1
+	movzx dx, dh			; Maximum head number
+	add dx, 1			; Head numbers start at 0 - add 1 for total
+	mov [Sides], dx
     
 
 
@@ -23,15 +36,37 @@ cls:
     int 0x10
     ret
 
+	cmp dl, 0
+	je no_change
+	mov [bootdev], dl		; Save boot device number
+	push es
+	mov ah, 8			; Get drive parameters
+	int 13h
+	pop es
+	and cx, 3Fh			; Maximum sector number
+	mov [SecsPerTrack], cx		; Sector numbers start at 1
+	movzx dx, dh			; Maximum head number
+	add dx, 1			; Head numbers start at 0 - add 1 for total
+	mov [Sides], dx
 
-run_prgm:
-	call cls
-	xor ax, ax
-	xor bx, vx
-	xor cx, cx
-	xor dx, dx
-	xor si, si
-	xor di, di
-	call 32768
-	call cls
-    jmp $
+no_change:
+	mov ax, 1003h			; Set text output with certain attributes
+	mov bx, 0			; to be bright, and not blinking
+	int 10h
+
+	
+	; Let's see if there's a file called AUTORUN.BIN and execute
+	; it if so, before going to the program launcher menu
+
+	mov ax, autorun_bin_file_name
+	call os_file_exists
+	jc no_autorun_bin		; Skip next three lines if AUTORUN.BIN doesn't exist
+
+	mov cx, 32768			; Otherwise load the program into RAM...
+	call os_load_file
+	jmp execute_bin_program		; ...and move on to the executing part
+	
+	
+	
+	%include src/disk.asm
+
