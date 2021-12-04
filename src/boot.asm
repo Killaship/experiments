81ef28a550f31b0c8194630c7fbf1f605ac13142
Killaship <69988679+Killaship@users.noreply.github.com>
2021-12-04 10:11:31 -0500

    Update boot.asm

--- src/boot.asm
+++ src/boot.asm
@@ -10,70 +10,12 @@ org 0x7C00
     mov ch, 0
     mov dh, 0
     mov cl, 2
-    mov bx, boot
+    mov bx, 0x1000
     int 0x13
 
-    jmp boot
+    jmp 0x1000
 
     ; Magic bytes.    
     times ((0x200 - 2) - ($ - $$)) db 0x00
     dw 0xAA55
 
-
-boot:
-	mov ax, 0x2401
-	int 0x15
-	mov ax, 0x3
-	int 0x10
-	cli
-	lgdt [gdt_pointer]
-	mov eax, cr0
-	or eax,0x1
-	mov cr0, eax
-	jmp CODE_SEG:boot2
-gdt_start:
-	dq 0x0
-gdt_code:
-	dw 0xFFFF
-	dw 0x0
-	db 0x0
-	db 10011010b
-	db 11001111b
-	db 0x0
-gdt_data:
-	dw 0xFFFF
-	dw 0x0
-	db 0x0
-	db 10010010b
-	db 11001111b
-	db 0x0
-gdt_end:
-gdt_pointer:
-	dw gdt_end - gdt_start
-	dd gdt_start
-
-CODE_SEG equ gdt_code - gdt_start
-DATA_SEG equ gdt_data - gdt_start
-
-bits 32
-boot2:
-	mov ax, DATA_SEG
-	mov ds, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
-	mov ss, ax
-	mov esi,hello
-	mov ebx,0xb8000
-.loop:
-	lodsb
-	or al,al
-	jz halt
-	or eax,0x0100
-	mov word [ebx], ax
-	add ebx,2
-	jmp .loop
-halt:
-	cli
-	hlt
-hello: db "Hello world!",0
