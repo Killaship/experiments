c38fd919041e9d28f8f2cb0952dc8a5af367c860
Killaship <69988679+Killaship@users.noreply.github.com>
2021-12-11 09:17:53 -0500

    Update and rename test.c to src/boot.c

--- test.c
+++
@@ -1,8 +0,0 @@
-__asm__(".code16\n");
-__asm__("jmpl $0x0000, $main\n");
-
-void main() {
-     __asm__ __volatile__ ("movb $'X'  , %al\n");
-     __asm__ __volatile__ ("movb $0x0e, %ah\n");
-     __asm__ __volatile__ ("int $0x10\n");
-}
