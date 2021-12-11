3f9e91aeb1bd02b524d7f61484b7ef4914852964
Killaship <69988679+Killaship@users.noreply.github.com>
2021-12-11 09:12:50 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 git pull
-gcc -m32 -c -g -Os -march=i686 -ffreestanding -Wall -Werror src/boot.c -o test.o
+gcc -c -g -Os -march=i686 -ffreestanding -Wall -Werror test.c -o test.o
 ld -static -Ttest.ld -nostdlib --nmagic -o test.elf test.o
 objcopy -O binary test.elf test.bin
 dd if=/dev/zero of=floppy.img bs=512 count=2880
