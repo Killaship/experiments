18c58fdb429d1ba78a9a724488d23f321b2af704
Killaship <69988679+Killaship@users.noreply.github.com>
2022-02-17 16:16:47 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,3 @@
 git pull
-gcc -c -g -Os -m32 -ffreestanding -Wall -Werror src/boot.c -o test.o
-ld -melf_i386 -static -Ttest.ld -nostdlib --nmagic -o test.elf test.o
-objcopy -O binary test.elf test.bin
-dd if=/dev/zero of=floppy.img bs=512 count=2880
-dd if=test.bin of=floppy.img
+
 qemu-system-i386 -fda floppy.img
